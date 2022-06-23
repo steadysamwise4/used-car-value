87f3e8c1a70db457932846b42f66ee77bf43c6fa
steadysamwise4 <davenportsam44@gmail.com>
2022-06-23 16:43:40 -0500

    Created Auth Service and integrated it with User Module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -11,5 +11,34 @@ export class UsersService {
     const user = this.repo.create({ email, password });
 
     return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    if (!id) {
+      throw new NotFoundException('user not signed in');
+    }
+    return this.repo.findOne(id);
+  }
+
+  find(email: string) {
+    return this.repo.find({ email });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    return this.repo.remove(user);
   }
 }
